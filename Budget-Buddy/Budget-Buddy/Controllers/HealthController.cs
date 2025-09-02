ea40289ad13fb8245cd9a39b4bc818cef13c697b
Johnathan Cruz <johnathancruz789@gmail.com>
2025-09-02 09:47:40 -0400

    Initial commit fixes #184

---
+++ Budget-Buddy/Budget-Buddy/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Budget_Buddy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController: ControllerBase
+
+    {
+        [HttpGet] public IActionResult Get() => Ok(new { status = "ok" });
+    }
+}

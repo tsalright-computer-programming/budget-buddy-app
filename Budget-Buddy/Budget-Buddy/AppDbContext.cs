0f41637643382f288b7558e1e94f00573b07a529
Johnathan Cruz <johnathancruz789@gmail.com>
2025-09-09 09:25:32 -0400

    Setting up base AppDbContext

---
+++ Budget-Buddy/Budget-Buddy/AppDbContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+}

8e0e2fd77b46a7d7d539c2eb8478fb0273dd9782
Johnathan Cruz <johnathancruz789@gmail.com>
2025-09-11 23:49:55 -0400

    Initial database connectivity established.

--- Budget-Buddy/Budget-Buddy/AppDbContext.cs
+++ Budget-Buddy/Budget-Buddy/AppDbContext.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Budget_Buddy.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Category> Categories { get; set; }
 }
